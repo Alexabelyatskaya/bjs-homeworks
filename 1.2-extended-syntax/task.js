e61ca5d389dc5cf810fc48947968371d9d394856
Alexandra <alexabelyatskaya@yandex.ru>
2020-03-30 12:03:36 +0300

    Правки

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,21 +2,22 @@
 
 function getResult(a,b,c){
 
-	var dis = Math.pow(b,2) - (4 * a * c);
+	let discriminant = Math.pow(b,2) - (4 * a * c);
+    let decision = [];
 
-	if (dis > 0) {
-	    var x1 = (- b + Math.sqrt(dis)) / 2 * a;
-        var x2 = (- b - Math.sqrt(dis)) / 2 * a;
-        var decision = [x1, x2];
+	if (discriminant > 0) {
+	    let x1 = (- b + Math.sqrt(discriminant)) / 2 * a;
+        let x2 = (- b - Math.sqrt(discriminant)) / 2 * a;
+        decision = [x1, x2];
 	} 
 
-	else if (dis == 0) {
-		var x = - b / (2 * a);
-		var decision = [x];
+	else if (discriminant == 0) {
+		let x = - b / (2 * a);
+		decision = [x];
 	}
 
-	else if (dis < 0) {
-		var decision = [];
+	else if (discriminant < 0) {
+		decision = [];
 	}
 
 	return decision;
@@ -26,42 +27,34 @@ function getResult(a,b,c){
 
 function getAverageMark(marks){
 
-    var summ = 0;
-    var array1 = marks.slice(0, 5);
-    console.log(array1);
+    let summ = 0;
+    let array1 = marks.slice(0, 5);
+    let average = 0;
 
  if (marks.length == 0) {
-    	var a = 0;
+    	return average;
     } 
 
     else {
-
-	for (var i = 0; i < 5; i++) {
+    	for (var i = 0; i < array1.length; i++) {
 		summ = array1[i] + summ;
 	}
 
-	var a = summ / array1.length;
+    average = summ / array1.length;
+	return average;
     }
-
-	return a;
 }
 
 function getAge(year) {
 
-	var date1 = new Date();
-	var yearNow = date1.getFullYear();
-	var b = year.getFullYear();
-
-	return yearNow - b;
+	return new Date().getFullYear() - year.getFullYear();
 }
 
 function askDrink(name,dateOfBirthday){
 
     if (getAge(dateOfBirthday) < 18) {
-        var a = 'Сожалею, ' + name +', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!';
+        return text = 'Сожалею, ' + name +', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!';
     } else {
-    	var a = 'Не желаете ли олд-фэшн, ' + name + '?';
+    	return text = 'Не желаете ли олд-фэшн, ' + name + '?';
     }
-
-    return a;
 }
