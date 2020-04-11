da4950744200e37078b0eb243d68794fcf2f6e0b
Alexandra <alexabelyatskaya@yandex.ru>
2020-04-11 21:49:55 +0300

    Задание

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,40 @@
+// Задание 1
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+
+	if (animal === undefined) {
+
+		return null;
+	}
+
+	let sound = animal.sound;
+
+	return sound;
 }
 
+// Задание 2
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+	console.log(marks);
+
+    let summ = 0;
+    let array1 = marks;
+
+    if (marks.length == 0) {
+    	return 0;
+    } 
+
+    for (var i = 0; i < array1.length; i++) {
+	    summ = Number(array1[i]).valueOf() + summ;
+	
+    } 
+
+    let roundedAverage = Math.round(summ / array1.length);
+
+    console.log(roundedAverage);
+
+	return roundedAverage;
 }
 
 function checkBirthday(birthday) {
